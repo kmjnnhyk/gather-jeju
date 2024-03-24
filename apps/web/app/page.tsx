f5b45a7ad909a3c7323924e22187beb896983a31
실비아헬스 <silviahealth@silbiahelseuui-MacBookPro.local>
2024-03-24 23:57:08 +0900

    fix: fix web build error by lint

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,11 @@ import ClientSide from './Clientside';
 import { trpc } from './trpc';
 
 export default async function Home() {
-  const  response =   await trpc.hello.query({name: 'jinhyeok'})
+  const response = await trpc.hello.query({ name: 'jinhyeok' });
   return (
-    <div>{response}<ClientSide/></div>
+    <div>
+      {response}
+      <ClientSide />
+    </div>
   );
 }
