0627f4212a6d784d149bbdb284bf3339db339490
실비아헬스 <silviahealth@silbiahelseuui-MacBookPro.local>
2024-03-24 23:38:44 +0900

    feat: init nest and next monorepo structure and eslint

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import ClientSide from './Clientside';
+import { trpc } from './trpc';
+
+export default async function Home() {
+  const  response =   await trpc.hello.query({name: 'jinhyeok'})
+  return (
+    <div>{response}<ClientSide/></div>
+  );
+}
